6204bd91e7a82cda38f36b67d651606d247958d8
Lijuan Xu <lijuanxu@hotmail.com>
2022-12-28 20:04:41 -0600

    update cart

--- react-app/src/components/Cart/UpdateCart.js
+++ react-app/src/components/Cart/UpdateCart.js
@@ -13,32 +13,28 @@ function UpdateCart({cart}) {
         quantity
     }
 
-
-    // useEffect(() => {
-    //     dispatch(updateCart(cart.id, payload));
-    // }, [dispatch, payload]);
-
+    useEffect((quantity) => {
+        dispatch(updateCart(cart.id, payload))
+    }, [quantity])
 
     return (
         <div>
-            <form method="post">
-                <select
-                    name='quantity'
-                    onChange={(e) => setQuantity(e.target.value)}
-                    value={quantity}
-                >
-                    {/* [...Array(11).keys()] is [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] */}
-                    {[...Array(11).keys()].slice(1).map((num) => (
-                        <option
-                            key={num}
-                            value={num}
-                        >
-                            {num}</option>
-                    ))}
-                </select>
-            </form>
+            <select
+                name='quantity'
+                onChange={(e) => setQuantity(e.target.value)}
+                value={quantity}
+            >
+                {/* [...Array(11).keys()] is [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] */}
+                {[...Array(11).keys()].slice(1).map((num) => (
+                    <option
+                        key={num}
+                        value={num}
+                    >
+                        {num}</option>
+                ))}
+            </select>
             <div>
-                {`$${(cart.quantity * cart.Item.price).toFixed(2)}`}
+                {`$${(quantity * cart.Item.price).toFixed(2)}`}
             </div>
         </div>
     )
