bbf88967022c11a4d7e5e6e7b5d5bebd1dbcd0f7
Lijuan Xu <lijuanxu@hotmail.com>
2022-12-29 22:44:04 -0600

    footer css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,6 +12,8 @@ import Homepage from './components/Homepage/Homepage';
 import MyListing from './components/Product/MyLisitng';
 import ProductDetail from './components/Product/ProductDetails';
 import MyCart from './components/Cart/Cart';
+import Footer from './components/Footer/Footer';
+import './index.css'
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -29,38 +31,41 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/products/current' exact={true} >
-          <MyListing />
-        </ProtectedRoute>
-        <Route path='/products/:productId' exact={true} >
-          <ProductDetail />
-        </Route>
-        <ProtectedRoute path='/carts' exact={true} >
-          <MyCart />
-        </ProtectedRoute>
-        <Route path='/' exact={true} >
-          <Homepage />
-        </Route>
-        <Route path='/coming-soon' exact={true} >
-          <h1>Feature coming soon!</h1>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+      <div className='page-container'>
+          <NavBar />
+          <Switch>
+            <Route path='/login' exact={true}>
+              <LoginForm />
+            </Route>
+            <Route path='/sign-up' exact={true}>
+              <SignUpForm />
+            </Route>
+            <ProtectedRoute path='/users' exact={true} >
+              <UsersList/>
+            </ProtectedRoute>
+            <ProtectedRoute path='/users/:userId' exact={true} >
+              <User />
+            </ProtectedRoute>
+            <ProtectedRoute path='/products/current' exact={true} >
+              <MyListing />
+            </ProtectedRoute>
+            <Route path='/products/:productId' exact={true} >
+              <ProductDetail />
+            </Route>
+            <ProtectedRoute path='/carts' exact={true} >
+              <MyCart />
+            </ProtectedRoute>
+            <Route path='/' exact={true} >
+              <Homepage />
+            </Route>
+            <Route path='/coming-soon' exact={true} >
+              <h1>Feature coming soon!</h1>
+            </Route>
+          </Switch>
+          <Footer />
+          </div>
+        </BrowserRouter>
   );
 }
 
--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -3,14 +3,16 @@ import linkedin from '../../assets/linkedin.png'
 import './footer.css'
 
 const Footer = () => {
-    <div className="developer-info">Developer:  Lijuan Xu
-                <a href='https://github.com/XU1204'>
-                    <img className='github-icon' src={github} alt='github'></img>
-                </a>
-                <a href='https://www.linkedin.com/in/lijuan-xu-96151b146/'>
-                    <img className='linkedin-icon' src={linkedin} alt='linkedin'></img>
-                </a>
-    </div>
+    return (
+        <div className="developer-info">Developer:  Lijuan Xu
+                    <a href='https://github.com/XU1204'>
+                        <img className='github-icon' src={github} alt='github'></img>
+                    </a>
+                    <a href='https://www.linkedin.com/in/lijuan-xu-96151b146/'>
+                        <img className='linkedin-icon' src={linkedin} alt='linkedin'></img>
+                    </a>
+        </div>
+    )
 }
 
 export default Footer;
