b98a3cbd53d15db163b8e4beabaa2c6c16eba085
Lijuan Xu <lijuanxu@hotmail.com>
2022-12-31 19:32:21 -0600

    add to cart

--- react-app/src/components/Cart/CreateCart.js
+++ react-app/src/components/Cart/CreateCart.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { useDispatch } from "react-redux";
-import { createCart } from '../../store/cart';
+import { createCart, updateCart } from '../../store/cart';
 import { useEffect } from 'react';
 import './cart.css'
 import { useHistory } from 'react-router-dom';
 
-function CreateCart({product}) {
+function CreateCart({product, isExist}) {
     const dispatch = useDispatch()
     const history = useHistory()
 
@@ -17,7 +17,18 @@ function CreateCart({product}) {
             itemId: product.id,
             quantity
         }
-        await dispatch(createCart(payload))
+        const payload2 = {
+            quantity: isExist.quantity + quantity
+        }
+
+        // if the item is not in your cart, then add this new item to your cart
+        if (!isExist) {
+            await dispatch(createCart(payload))
+        // if the item is already in your cart, just update the quantity
+        } else {
+            await dispatch(updateCart(isExist.id, payload2))
+        }
+
         history.push('/carts')
     }
     return (
--- react-app/src/components/Product/ProductDetails.js
+++ react-app/src/components/Product/ProductDetails.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProducts } from "../../store/product";
+import { getCarts } from "../../store/cart";
 import CreateCart from "../Cart/CreateCart";
 import './product.css'
 
@@ -12,17 +13,23 @@ function ProductDetail () {
 
     useEffect(() => {
         dispatch(getProducts())
+        dispatch(getCarts())
     }, [dispatch])
 
     const allProducts = useSelector(state => Object.values(state.products))
     const product = allProducts.find(product => product.id === +productId)
+    const carts = useSelector(state => Object.values(state.carts))
     if(!product) return null
+    if (!carts) return null;
+
+    const isExist = carts.find(cart => cart.itemId === +productId)
+
 
     return (
         <>
             <h1>detail</h1>
             <img src={product.previewImage}></img>
-            <CreateCart product={product} />
+            <CreateCart product={product} isExist={isExist}/>
         </>
     )
 }
